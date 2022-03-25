451a470f60d7916b8003c5b4aa0bdc24c30dc8b0
Anzhy11 <harnzzy@gmail.com>
2022-03-25 06:27:47 -0700

    Added Sidebar Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
+
+      <div className="app__body">
+        <Sidebar />
+        {/* feed
+        widgets */}
+      </div>
     </div>
   );
 }
